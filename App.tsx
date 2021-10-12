bfb25f59f37c91494cc61abc72733aa6ac28fc93
bbangaro <bbangaro@gmail.com>
2021-10-12 18:19:32 +0900

    flatList test

--- App.tsx
+++ App.tsx
@@ -1,119 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- *//*
-
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
-*/
-import React from 'react';
-import {View, Text, FlatList, ListRenderItem} from 'react-native';
+import {View, StyleSheet, Text, FlatList, ListRenderItem} from 'react-native';
 import styled from 'styled-components/native';
 //import {StyleSheet} from 'react-native';
 //import TextItem from './components/TextItem';
@@ -122,9 +8,9 @@ type ListItem = {id: string, text: string}
 type List = ListItem[];
 
 const list: List = [
-  {id: "1", text: "투수리스트를 만들꼬야!"},
-  {id: "2", text: "투수리스트를 만들수 있을까..?"},
-  {id: "3", text: "투수리스트를 어떻게든 만들자!"},
+  {id: "1", text: "원두"},
+  {id: "2", text: "투두"},
+  {id: "3", text: "쓰리두"},
 ];
 
 
@@ -140,6 +26,9 @@ function App() {
   return (
     <Container>
       <TodoList
+        style={{ backgroundColor: 'ivory', height: '100%' }}
+        contentContainerStyle={{ padding: 50, }}
+
         data={list}
         renderItem={renderItem}
         keyExtractor={(item: ListItem)  => item.id}
@@ -159,6 +48,12 @@ const Container = styled.View`
   padding-top: 50px;
 `
 
+const styles = StyleSheet.create({
+  tab_1_item: {
+    fontSize: 20,
+  },
+});
+
 export default App;
 
 /* function App() {
@@ -173,6 +68,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems:'center'
+    alignItems:'center',
   },
 }); */
