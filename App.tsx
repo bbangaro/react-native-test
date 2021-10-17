cfcf6bb0d05d443bce03783eb141167d429ebea4
bbangaro <bbangaro@gmail.com>
2021-10-17 22:48:41 +0900

    rn style 적용해보기

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {View, StyleSheet, Text, FlatList, ListRenderItem} from 'react-native';
+import {View, SafeAreaView, StyleSheet, Text, FlatList, Image, ListRenderItem} from 'react-native';
 import styled from 'styled-components/native';
-//import {StyleSheet} from 'react-native';
 //import TextItem from './components/TextItem';
 
+/*
 type ListItem = {id: string, text: string}
 type List = ListItem[];
 
@@ -12,62 +12,118 @@ const list: List = [
   {id: "2", text: "투두"},
   {id: "3", text: "쓰리두"},
 ];
+*/
+
+const data = [
+  {
+    id: 1,
+    uri: 'https://i.ytimg.com/vi/ZYvvmsrDOj8/maxresdefault.jpg'
+  },
+  {
+    id: 2,
+    uri: 'https://i.ytimg.com/vi/ZYvvmsrDOj8/maxresdefault.jpg'
+  },
+  {
+    id: 3,
+    uri: 'https://i.ytimg.com/vi/ZYvvmsrDOj8/maxresdefault.jpg'
+  },
+  {
+    id: 4,
+    uri: 'https://i.ytimg.com/vi/ZYvvmsrDOj8/maxresdefault.jpg'
+  },
+  {
+    id: 5,
+    uri: 'https://i.ytimg.com/vi/ZYvvmsrDOj8/maxresdefault.jpg'
+  },
+  {
+    id: 6,
+    uri: 'https://i.ytimg.com/vi/ZYvvmsrDOj8/maxresdefault.jpg'
+  },
+  {
+    id: 7,
+    uri: 'https://i.ytimg.com/vi/ZYvvmsrDOj8/maxresdefault.jpg'
+  },
+
+]
 
 
 function App() {
-  const renderItem: ListRenderItem<ListItem> = ({item}) => {
+  /*const renderItem: ListRenderItem<ListItem> = ({item}) => {
     return (
-        <View>
+        <View style={styles.item}>
           <Text>{item.text}</Text>
         </View>
     );
-  };
+  };*/
 
   return (
-    <Container>
-      <TodoList
-        style={{ backgroundColor: 'ivory', height: '100%' }}
-        contentContainerStyle={{ padding: 50, }}
+    <SafeAreaView style={styles.wrap}>
+      <FlatList
 
-        data={list}
-        renderItem={renderItem}
-        keyExtractor={(item: ListItem)  => item.id}
+        data={data}
+        ListHeaderComponent={()=>
+          <View style={styles.header}>
+            <Text style={styles.headerText}>Header</Text>
+          </View>
+        }
+        renderItem={({ item })=>
+          <View style={styles.imageContainer}>
+            <Image style={styles.image} source={{ uri: item.uri }} />
+          </View>
+        }
+        columnWrapperStyle={styles.imageRow}
+        numColumns={3}
       />
-    </Container>
+    </SafeAreaView>
   );
 }
 
+/*
 const TodoList = (styled.FlatList`
   width: 100%;
 ` as unknown) as typeof FlatList;
-
-const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  padding-top: 50px;
-`
-
+*/
 const styles = StyleSheet.create({
-  tab_1_item: {
-    fontSize: 20,
-  },
+    wrap: {
+      flex: 1,
+      minWidth: 360,
+      width: 365,
+      minHeight: 700,
+      margin: 'auto',
+      border: '1px solid #d3d3d3',
+    },
+    header: {
+      height: 60,
+      borderBottomWidth: 2,
+      borderBottomColor: '#d3d3d3',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    headerText: {
+      fontSize: 20,
+      fontWeight: 'bold',
+    },
+    imageRow: {
+      justifyContent: 'space-between',
+    },
+    imageContainer : {
+        width: 120,
+        height: 120,
+    },
+    image: {
+      //width: 120,
+      //height: 120,
+      height:'100%',
+      width:'100%',
+      resizeMode:'cover'
+    },
 });
+
+/*
+const Container = styled.View`
+  margin-top: 50px;
+`
+*/
 
 export default App;
 
-/* function App() {
-  return (
-    <View style={styles.container}>
-      <TextItem text="Hello world from React Naitve Web!" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems:'center',
-  },
-}); */
